b51ff82c463d26d6873a0139e221d6030835bef6
Kirk <kkchu791@gmail.com>
2015-12-25 01:39:07 -0800

    Issue#9 Allow user to demo app before sign up
    Add ability to let the user play around before
    they become a member because it's a hassle to
    have to always sign up before seeing the app.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user, except: [:index]
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   # GET  /topics topics_path
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,5 @@
 class TopicsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user, except: [:index, :new, :edit, :create, :update, :upvote, :destroy]
   before_action :set_topic, only: [:show, :edit, :update, :destroy]
 
   # GET  /topics topics_path
